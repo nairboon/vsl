058657127cfa89bf9a99596c14b5c542389f3e15
Remo Hertig <remo.hertig@bluewin.ch>
2013-10-29 16:17:43 +0100

    Initial Commit

---
+++ public/js/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+// Declare app level module which depends on filters, and services
+
+angular.module('myApp', [
+  'myApp.controllers',
+  'myApp.filters',
+  'myApp.services',
+  'myApp.directives',
+
+  // 3rd party dependencies
+  'btford.socket-io'
+]).
+config(function ($routeProvider, $locationProvider) {
+  $routeProvider.
+    when('/projects', {
+      templateUrl: 'partials/partial1',
+      controller: 'ProjectsCtrl'
+    }).
+    when('/models', {
+      templateUrl: 'partials/partial2',
+      controller: 'MyCtrl2'
+    }).
+    when('/experiments', {
+      templateUrl: 'partials/partial2',
+      controller: 'MyCtrl2'
+    }).
+    when('/runs', {
+      templateUrl: 'partials/partial2',
+      controller: 'MyCtrl2'
+    }).
+    otherwise({
+      redirectTo: '/projects'
+    });
+
+  $locationProvider.html5Mode(true);
+});
